448999ca6d930a46b9394940fd998b7d2a33dab1
Luis Aguilar Rojas <luis.rojas@suse.com>
2023-08-26 13:39:50 -0600

    Update hideLicense.js

--- hideLicense.js
+++ hideLicense.js
@@ -1,12 +1,8 @@
 function hideLicense() {
-  var elements = document.querySelectorAll('license_system-messages-wrapper__2jNgH');
-  for (var i = 0, l = elements.length; i < l; i++) {
-    subElement = elements[i].querySelector(':scope license_system-messages-wrapper__2jNgH');
-    if (subElement.firstChild.tagName != 'A') {
-      elements[i].style.display = "none";
-    }
-  }
+var appBanners = document.getElementsByClassName('license_system-messages-wrapper__2jNgH');
+for (var i = 0; i < appBanners.length; i ++) {
+    appBanners[i].style.display = 'none';
 }
-
+}
 //delay
 setTimeout(hideLicense, 1000);
