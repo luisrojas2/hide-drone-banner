6790b85ad8304342f83d6025005840e19f94132f
Luis Aguilar Rojas <luis.rojas@suse.com>
2023-08-26 13:34:03 -0600

    Update hideLicense.js

--- hideLicense.js
+++ hideLicense.js
@@ -1,13 +1,12 @@
-function hideCards() {
-  var elements = document.querySelectorAll('div.card');
+function hideLicense() {
+  var elements = document.querySelectorAll('license_system-messages-wrapper__2jNgH');
   for (var i = 0, l = elements.length; i < l; i++) {
-    subElement = elements[i].querySelector(':scope div.card-body');
+    subElement = elements[i].querySelector(':scope license_system-messages-wrapper__2jNgH');
     if (subElement.firstChild.tagName != 'A') {
       elements[i].style.display = "none";
     }
   }
 }
 
-// execute after a second to give the page time to create the
-// elements you want to remove
-setTimeout(hideCards, 1000);
+//delay
+setTimeout(hideLicense, 1000);
